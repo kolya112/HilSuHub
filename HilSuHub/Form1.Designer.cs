278a5303d1e27d97438373254d0532ea340446f5
kolya112 <nikolayurch@gmail.com>
2021-12-20 23:55:40 +0300

    Add top 1 in coins

--- HilSuHub/Form1.Designer.cs
+++ HilSuHub/Form1.Designer.cs
@@ -30,16 +30,20 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.top1csum = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.top1cnick = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Location = new System.Drawing.Point(85, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 0;
@@ -47,21 +51,13 @@
             // 
             // panel1
             // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(12, 25);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(299, 413);
             this.panel1.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "HiTech";
             // 
             // label3
             // 
@@ -73,21 +69,60 @@
             this.label3.Text = "Sandbox";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(434, 151);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "label4";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "HiTech";
+            // 
+            // top1csum
+            // 
+            this.top1csum.AutoSize = true;
+            this.top1csum.Location = new System.Drawing.Point(102, 31);
+            this.top1csum.Name = "top1csum";
+            this.top1csum.Size = new System.Drawing.Size(140, 13);
+            this.top1csum.TabIndex = 2;
+            this.top1csum.Text = "Сумма личного состояния";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.top1cnick);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.top1csum);
+            this.panel2.Location = new System.Drawing.Point(317, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(471, 413);
+            this.panel2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(34, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Топ 1 по червонцам";
+            // 
+            // top1cnick
+            // 
+            this.top1cnick.AutoSize = true;
+            this.top1cnick.Location = new System.Drawing.Point(16, 31);
+            this.top1cnick.Name = "top1cnick";
+            this.top1cnick.Size = new System.Drawing.Size(71, 13);
+            this.top1cnick.TabIndex = 4;
+            this.top1cnick.Text = "Игровой ник";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
@@ -97,6 +132,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,7 +145,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label top1csum;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label top1cnick;
     }
 }
 
--- HilSuHub/Form1.cs
+++ HilSuHub/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace HilSuHub
 {
@@ -28,16 +29,33 @@ namespace HilSuHub
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            HttpWebResponse response = (HttpWebResponse)WebRequest.Create($"https://api.hil.su/v2/economy/top?limit=1&currency=coins").GetResponse();
-            JSON coinstopone = JsonConvert.DeserializeObject<JSON>(new StreamReader(response.GetResponseStream()).ReadToEnd());
-            response.Close();
+            // Топ 1 по червонцам
+            HttpWebResponse response1 = (HttpWebResponse)WebRequest.Create($"https://api.hil.su/v2/economy/top?limit=1&currency=coins").GetResponse(); // Первый запрос
 
-            label4.Text = "Червонцев: " + coinstopone.balance;
+            JSON apiAVAILABLE = JsonConvert.DeserializeObject<JSON>(new StreamReader(response1.GetResponseStream()).ReadToEnd());
+
+            if (apiAVAILABLE.success != true)
+            {
+                top1csum.Text = "Ошибка подключения к HilAPI";
+                response1.Close();
+            }
+            else
+            {
+                HttpWebResponse response2 = (HttpWebResponse)WebRequest.Create($"https://api.hil.su/v2/economy/top?limit=1&currency=coins").GetResponse(); // Второй запрос
+                string serveranswer2 = new StreamReader(response2.GetResponseStream()).ReadToEnd();
+                string resultbalance = JObject.Parse(serveranswer2).SelectToken("response.users[0].balance").ToString();
+                string resultnickname = JObject.Parse(serveranswer2).SelectToken("response.users[0].user.username").ToString();
+
+                response1.Close();
+                response2.Close();
+                top1cnick.Text = resultnickname;
+                top1csum.Text = resultbalance;
+            }
         }
 
         class JSON
         {
-            public decimal balance { get; set; }
+            public bool success { get; set; }
         }
     }
 }
